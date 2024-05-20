7d40159845527288de614c9fddaec221fa4c1edf
Vo-Hoang-Minh-Sang <sangvo0339958048@gmail.com>
2024-05-20 15:30:06 +0700

    code

---
+++ src/main/java/org/githubactionsarchitecture/GithubActionsArchitectureApplication.java
@@ -0,0 +1,22 @@
+package org.githubactionsarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GithubActionsArchitectureApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to GitHub action";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/githubactionsarchitecture/GithubActionsArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.githubactionsarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
