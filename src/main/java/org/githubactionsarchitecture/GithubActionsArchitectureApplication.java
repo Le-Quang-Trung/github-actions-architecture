021204cfd6262fed465d93286bd771792017052b
trung <leqtrungggg3105@gmail.com>
2024-05-22 11:00:42 +0700

    change code

--- src/main/java/org/githubactionsarchitecture/GithubActionsArchitectureApplication.java
+++ src/main/java/org/githubactionsarchitecture/GithubActionsArchitectureApplication.java
@@ -11,7 +11,7 @@ public class GithubActionsArchitectureApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to GitHub action";
+		return "Welcome to GitHub ";
 	}
 
 
